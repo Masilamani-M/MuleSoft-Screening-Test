22169441b11840c26d45d39a5131201b7ae89a02
Masilamani <108478190+Masilamani-M@users.noreply.github.com>
2022-06-30 21:24:13 +0530

    Update Masilamani_MulesoftScreening.py

--- Masilamani_MulesoftScreening.py
+++ Masilamani_MulesoftScreening.py
@@ -4,9 +4,9 @@ cursor = connection.cursor()
 cursor.execute('''CREATE TABLE IF NOT EXISTS Mulesoft
               (MovieName TEXT,Actor TEXT,Actress TEXT, Year INT,Director TEXT)''')
 cursor.execute('''INSERT INTO Mulesoft (MovieName,Actor,Actress,Year,Director) VALUES ('Vikram', 'Kamal Haasan', '-', 2022,'Lokesh Kanagaraj' )''')
-cursor.execute('''INSERT INTO Mulesoft (MovieName,Actor,Actress,Year,Director)VALUES ('Master', 'Vijay', 'Malvika Mohan', 2021,'Lokesh Kanagaraj' )''')
-cursor.execute('''INSERT INTO Mulesoft (MovieName,Actor,Actress,Year,Director)VALUES ('Master', 'Vijay Sethupathi', '', 2021,'Loki' )''')
-cursor.execute('''INSERT INTO Mulesoft (MovieName,Actor,Actress,Year,Director)VALUES ('Red', 'AK', 'Sai', 2001,'Noper' )''')
+cursor.execute('''INSERT INTO Mulesoft (MovieName,Actor,Actress,Year,Director)VALUES ('Master', 'Vijay', 'Malavika Mohan', 2021,'Lokesh Kanagaraj' )''')
+cursor.execute('''INSERT INTO Mulesoft (MovieName,Actor,Actress,Year,Director)VALUES ('Don', 'Sivakarthikeyan', 'Priyanka Mohan', 2022,'Cibi Chakaravarthi' )''')
+cursor.execute('''INSERT INTO Mulesoft (MovieName,Actor,Actress,Year,Director)VALUES ('Beast', 'Vijay', 'Pooja Hegde', 2022,'Nelson' )''')
 cursor.execute('''SELECT * from Mulesoft''')
 result = cursor.fetchall();
 print(result)
